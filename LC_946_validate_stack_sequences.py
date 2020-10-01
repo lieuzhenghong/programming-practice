cc1cead66da77e40afef23bdacac9798877a9ea0
Zhenghong Lieu <lieu@pop-os.localdomain>
2020-10-01 17:19:30 +0800

    Reattempted LC946 Validate Stack Sequences

--- LC_946_validate_stack_sequences.py
+++ LC_946_validate_stack_sequences.py
@@ -37,6 +37,10 @@ Review this question and do again with Pythonic data structures
 '''
 
 
+from typing import List
+from collections import deque
+
+
 class Solution:
     def validateStackSequences(self, pushed: List[int], popped: List[int]) -> bool:
         stack = []
@@ -55,3 +59,77 @@ class Solution:
                 stack = stack[:-1]  # pop last element
                 continue
         return True
+
+# Reattempted the solution 1st October 2020, 1700
+# Solved AC 1717
+
+
+class Solution:
+    def validateStackSequences(self, pushed: List[int], popped: List[int]) -> bool:
+        stack = deque([])
+        l = 0
+        for ele in popped:
+            while len(stack) == 0 or stack[-1] != ele:
+                if l == len(pushed):
+                    return False
+                stack.append(pushed[l])
+                l += 1
+                if stack[-1] == ele:
+                    break
+            if stack[-1] == ele:
+                stack.pop()
+                continue
+
+        return True
+
+
+'''
+Let's trace
+# Input: pushed = [1,2,3,4,5], popped = [4,5,3,2,1]
+[]
+ele = 4
+    while len(pushed) == 0 or stack[-1] != ele:
+        len(pushed == 0)
+        l != len(pushed)
+        stack.append(1)
+        stack = [1]
+        l = 1
+
+        stack[-1] = 1 != 4
+        l != len(pushed)
+        stack.append(2)
+        stack = [1, 2]
+        l = 2
+
+        stack[-1] = 2 != 4
+        l != len(pushed)
+        stack.append(3)
+        stack = [1, 2, 3]
+        l = 3
+
+        stack[-1] = 3 != 4
+        l != len(pushed)
+        stack.append(4)
+        stack = [1, 2, 3, 4]
+        l = 4
+        stack[-1] == 4 == 4
+            stack.pop() # stack = [1,2,3]
+            continue
+
+ele = 5
+    l = 4
+    stack[-1] == 4 != 5
+    stack.append(pushed[4]) # stack = [1,2,3,5]
+    l = 5
+    stack[-1] = 5 == ele
+    stack.pop() # stack = [1,2,3]
+
+ele = 3
+    l = 5
+    stack[-1] == 3
+'''
+
+assert(Solution().validateStackSequences(
+    pushed=[1, 2, 3, 4, 5], popped=[4, 5, 3, 2, 1]) is True)
+assert(Solution().validateStackSequences(
+    pushed=[1, 2, 3, 4, 5], popped=[4, 3, 5, 1, 2]) is False)
